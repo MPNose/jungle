99dd234a80c9c7799263e6a919e16de46c20fd8a
MPNose <mpnoseworthy89@gmail.com>
2024-12-12 16:03:12 -0800

    implement counter on admin dashboard.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @total_products = Product.count
+    @total_product_quantity = Product.sum('quantity')
+    @total_categories = Category.count
   end
 end
