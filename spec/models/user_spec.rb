986351d938f9f21b2058159cd651c66132b1534e
MPNose <mpnoseworthy89@gmail.com>
2024-12-17 13:37:58 -0800

    first validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      user = User.new(first_name: 'John', last_name: 'Doe', email: 'john@example.com', password: 'password', password_confirmation: 'password')
+      expect(user).to be_valid
+    end
+  end
 end
