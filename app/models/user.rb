6844cd4d2aa07bf836f7f458f306818a1ff6712f
MPNose <mpnoseworthy89@gmail.com>
2024-12-17 12:53:06 -0800

    change db to take first name and last name, add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :password, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
